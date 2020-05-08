c8a09a0e0511cebd95e99262aff122f29c31a261
dev0912 <FB.dev0912@outlook.com>
2020-05-08 04:07:06 -0700

    Changing redirect_uri

--- src/container/pages/LoginInWithSalseForcePage/index.js
+++ src/container/pages/LoginInWithSalseForcePage/index.js
@@ -11,8 +11,7 @@ const LoginInWithSalseForcePage = ({ login }) => {
     const params = {
       client_id: '3MVG96hCAx1bhPaglDTyFCa4LhXrXpBHi7cHCTNOI0xtdA_6FsjUqdjAmcVFXfsTDonw7TOCcoXA_FKua5DiR',
       client_secret: '92D4E394B393940DF3592AFD15C59D5B0ED3080F71CD780E69EA8B6656AC9193',
-      // redirect_uri : 'https://localhost:3000/oauth/_callback',
-      redirect_uri : 'https://969327b7.ngrok.io/oauth/_callback',
+      redirect_uri : 'https://react-salescope.herokuapp.com/oauth/_callback',
       grant_type: 'password',
       username: 'salescope@cloudadvisory.io',
       password: 'Cloudadv2020$'
